f910523ba2064545b0c15b74d90ce15cfae0ce98
Suchithra2505 <147540982+Suchithra2505@users.noreply.github.com>
2024-04-17 11:32:07 +0000

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
 
 import React from "react";
 import './../styles/App.css';
-
+import { useState } from "react";
 const App = () => {
+  const [name, setName] = useState('');
   return (
     <div>
         {/* Do not remove the main div */}
+        <label>Enter your name :</label>
+        <input  onChange={(event) =>  setName('Hello '+ event.target.value + '!')} />
+        <p>{name}</p>
     </div>
   )
 }
